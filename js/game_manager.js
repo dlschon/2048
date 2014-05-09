db713db85b41856c64ff2831735bee6f5999b939
dlschon <dlschon0@gmail.com>
2014-05-09 12:21:15 -0500

    Update game_manager.js

--- js/game_manager.js
+++ js/game_manager.js
@@ -5,7 +5,7 @@ function GameManager(size, InputManager, Actuator, StorageManager) {
   this.actuator       = new Actuator;
 
   this.startTiles     = 1;
-
+  console.log("this thing is actually working");
   this.inputManager.on("move", this.move.bind(this));
   this.inputManager.on("restart", this.restart.bind(this));
   this.inputManager.on("keepPlaying", this.keepPlaying.bind(this));
