22d10db2c1990b476cec2bba9f05e540df822e8d
dlschon <dlschon0@gmail.com>
2014-05-13 10:43:21 -0500

    Update game_manager.js

--- js/game_manager.js
+++ js/game_manager.js
@@ -49,7 +49,7 @@ GameManager.prototype.setup = function () {
     this.keepPlaying = previousState.keepPlaying;
   } else {
     this.grid        = new Grid(this.size);
-    this.score       = 9007199254740992;
+    this.score       = 0;
     this.over        = false;
     this.won         = false;
     this.keepPlaying = false;
